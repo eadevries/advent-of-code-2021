5426738ffe6f09e79b9eab68a1d02852bde5b26a
Elliot de Vries <elliot.devries@protonmail.com>
2021-12-02 22:07:11 -0600

    Add utility function to process line-by-line data

--- utils/src/Util.hs
+++ utils/src/Util.hs
@@ -1,2 +1,6 @@
 module Util where
 
+readDataLines :: (String -> Maybe a) -> String -> Either String [a]
+readDataLines f s = case mapM f (lines s) of
+    Just xs -> Right xs
+    Nothing -> Left "Error parsing data"
