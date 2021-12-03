3573b148fae28e1064078539ff648554b6d7cffe
Elliot de Vries <elliot.devries@protonmail.com>
2021-12-03 06:38:24 -0600

    Remove some unnecessary duplication in Day 2 and reorganize

--- day02/lib/Day02.hs
+++ day02/lib/Day02.hs
@@ -7,15 +7,20 @@ import Data.Foldable (foldl')
 
 data Move = Horizontal Int | Depth Int deriving Show
 
-data Position = Position { hPos :: Int, depth :: Int } deriving Show
-
-initialPosition :: Position
-initialPosition = Position { hPos = 0, depth = 0 }
+data Position = Position { hPos :: Int, depth :: Int, aim :: Int } deriving Show
 
 day02Part1 :: String -> String
 day02Part1 s = case readDataLines readMove s of
     Right xs -> show . productOfCoords . reckon $ xs
     Left e   -> e
+
+day02Part2 :: String -> String
+day02Part2 s = case readDataLines readMove s of
+    Right xs -> show . productOfCoords . reckon' $ xs
+    Left e   -> e
+
+initialPosition :: Position
+initialPosition = Position { hPos = 0, depth = 0, aim = 0 }
 
 readDirection :: String -> Maybe (Int -> Move)
 readDirection "forward"  = Just Horizontal
@@ -33,28 +38,15 @@ makeMove :: Position -> Move -> Position
 makeMove p@(Position { hPos = h })  (Horizontal m) = p { hPos = h + m }
 makeMove p@(Position { depth = d }) (Depth m)      = p { depth = d + m }
 
+makeMove' :: Position -> Move -> Position
+makeMove' p@(Position { hPos = h, depth = d, aim = a })  (Horizontal m) = p { hPos = h + m, depth = d + a * m }
+makeMove' p@(Position { aim = a })                       (Depth m)      = p { aim = a + m }
+
 reckon :: [Move] -> Position
 reckon = foldl' makeMove initialPosition
 
+reckon' :: [Move] -> Position
+reckon' = foldl' makeMove' initialPosition
+
 productOfCoords :: Position -> Int
 productOfCoords (Position { hPos = h, depth = d }) = h * d
-
-data PositionAndAim = PositionAndAim { hPos' :: Int, depth' :: Int, aim' :: Int } deriving Show
-
-initialPositionAndAim :: PositionAndAim
-initialPositionAndAim = PositionAndAim { hPos' = 0, depth' = 0, aim' = 0 }
-
-day02Part2 :: String -> String
-day02Part2 s = case readDataLines readMove s of
-    Right xs -> show . productOfCoords' . reckon' $ xs
-    Left e   -> e
-
-makeMove' :: PositionAndAim -> Move -> PositionAndAim
-makeMove' p@(PositionAndAim { hPos' = h, depth' = d, aim' = a })  (Horizontal m) = p { hPos' = h + m, depth' = d + a * m }
-makeMove' p@(PositionAndAim { aim' = a })                         (Depth m)      = p { aim' = a + m }
-
-reckon' :: [Move] -> PositionAndAim
-reckon' = foldl' makeMove' initialPositionAndAim
-
-productOfCoords' :: PositionAndAim -> Int
-productOfCoords' (PositionAndAim { hPos' = h, depth' = d }) = h * d
