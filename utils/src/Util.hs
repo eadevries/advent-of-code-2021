001c025828ed12d868a4d49d53f4b49692c5733d
Elliot de Vries <elliot.devries@protonmail.com>
2021-12-05 17:26:42 -0600

    Add additional utility functions for running daily parts

--- utils/src/Util.hs
+++ utils/src/Util.hs
@@ -1,5 +1,16 @@
 module Util where
 
+runDay :: FilePath -> (String -> String) -> (String -> String) -> IO ()
+runDay inputFile part1 part2 = do
+    fileStr <- readFile inputFile
+    putStrLn $ "Part 1: " <> part1 fileStr
+    putStrLn $ "Part 2: " <> part2 fileStr
+
+runPart :: FilePath -> (String -> String) -> IO ()
+runPart inputFile part = do
+    fileStr <- readFile inputFile
+    putStrLn $ "Result: " <> part fileStr
+
 readDataLines :: (String -> Maybe a) -> String -> Either String [a]
 readDataLines f s = case mapM f (lines s) of
     Just xs -> Right xs
