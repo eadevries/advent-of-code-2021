e82493fb90cec307637bcca25375efb0f2b2f88f
Elliot de Vries <elliot.devries@protonmail.com>
2021-12-09 07:03:51 -0600

    Add CSV utility functions

--- utils/src/Util.hs
+++ utils/src/Util.hs
@@ -1,5 +1,7 @@
 module Util where
 
+import Data.List.Split (splitOn)
+
 runDay :: FilePath -> (String -> String) -> (String -> String) -> IO ()
 runDay inputFile part1 part2 = do
     fileStr <- readFile inputFile
@@ -15,6 +17,14 @@ readDataLines :: (String -> Maybe a) -> String -> Either String [a]
 readDataLines f s = case mapM f (lines s) of
     Just xs -> Right xs
     Nothing -> Left "Error parsing data"
+
+readCSVLine :: (String -> Maybe a) -> String -> Either String [a]
+readCSVLine f s = case mapM f (splitOn "," s) of
+    Just xs -> Right xs
+    Nothing -> Left "Error parsing CSV line"
+
+readCSVLines :: (String -> Maybe a) -> String -> Either String [[a]]
+readCSVLines f s = mapM (readCSVLine f) (lines s)
 
 headMaybe :: [a] -> Maybe a
 headMaybe []    = Nothing
